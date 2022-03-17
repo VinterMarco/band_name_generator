bcc772a47cababd201f2c275fe4446ed2285790d
Vinter Marco <95470600+VinterMarco@users.noreply.github.com>
2022-03-17 09:34:30 +0200

    Add files via upload

---
+++ band_name_generator.py
@@ -0,0 +1,9 @@
+import time
+
+print("Welcome to the band name generator")
+time.sleep(2)
+print("\n")
+city = input("Which city did you grow up in?:\n")
+pet = input("What is the name of a pet?:\n")
+time.sleep(1)
+print("Your band name could be " + city + " " + pet)
